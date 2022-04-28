49fb4ff8e0cdd66093c070d8ef3c663e544f78aa
manthan ankolekar <manthan.ank46@gmail.com>
2022-04-28 20:19:35 +0530

    few changes made.

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3005
-
-app.use(express.static(path.join(__dirname, "public")))
-
+app.use(express.static(__dirname+'/public'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
